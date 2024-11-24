24b667e2072b98dce09a8b047f28b8f9fef8f65e
Aksel Hundstuen <aksel_hundstuen@hotmail.com>
2024-11-24 13:27:18 +0100

    Updated code so weather function app actually works with web app

--- JAHttpFunctionApp/Function1.cs
+++ JAHttpFunctionApp/Function1.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace JAHttpFunctionApp
 {
@@ -14,11 +17,27 @@ namespace JAHttpFunctionApp
             _logger = logger;
         }
 
+        //async static Task<string> GetResponseString()
+        async static Task<string> GetResponseString(string latitude, string longitude)
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "my-user-agent-name");
+
+            var response = await httpClient.GetAsync($"https://api.met.no/weatherapi/locationforecast/2.0/classic?lat={latitude}&lon={longitude}");
+            var contents = await response.Content.ReadAsStringAsync();
+
+            return contents;
+        }
+        
         [Function("Function1")]
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            Task<string> result = GetResponseString(req.Query["lat"], req.Query["lon"]);
+            var finalResult = result.Result;
+            var rootElement = XElement.Parse(finalResult);
+            var temperature = rootElement.Element("product").Element("time").Element("location").Element("temperature").Attribute("value").Value;
+
+            return new OkObjectResult(temperature);
         }
     }
 }
--- JAWebApp/Pages/Weather.cshtml.cs
+++ JAWebApp/Pages/Weather.cshtml.cs
@@ -3,35 +3,47 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Net;
 using System.Net.Http;
+using System.Xml.Linq;
 
 
 namespace JAWebApp.Pages
 {
     public class WeatherModel : PageModel
     {
-        [BindProperty]
-        public CoordinatesModel Coordinates { get; set; }
-
         private readonly ILogger<WeatherModel> _logger;
+
         public WeatherModel(ILogger<WeatherModel> logger)
         {
             _logger = logger;
         }
+
         public void OnGet()
         {
 
         }
 
-        public IActionResult OnPost()
+        async static Task<HttpResponseMessage> GetResponseString(string Latitude, string Longitude)
         {
-            if (ModelState.IsValid == false)
-            {
-                return Page();
-            }
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "my-user-agent-name");
+            var response = await httpClient.GetAsync($"https://jahttpfunctionapp.azurewebsites.net/api/Function1?code=-VEE_oXc5Y1Zx5OlSzy7lv6-gOO_8Za0G8Il0w51erZoAzFuTypQWw%3D%3D&lat={Latitude}&lon={Longitude}");
+            return response;
 
-            // Save Model to Database
+        }
+        public string ActionResultMessageText { get; set; }
+        public string ActionResultAlertMessageText { get; set; }
+        public async void OnPost()
+        {
+            var latitudeVar = Request.Form["latitude"];
+            var longitudeVar = Request.Form["longitude"];
 
-            return RedirectToPage("/Index", new {Coordinates.Latitude});
+            var temperature = float.Parse(GetResponseString(latitudeVar, longitudeVar).Result.Content.ReadAsStringAsync().Result);
+
+            var jackets = temperature > 15 ? 0 : Math.Floor((15 - temperature) / 10);
+            this.ActionResultMessageText = string.Empty;
+            this.ActionResultAlertMessageText = jackets == 0 ?
+                $"It is {temperature} degrees. You do not need a jacket" :
+                jackets == 1 ? $"It is {temperature} degrees. You need a jacket" : $"It is {temperature} degrees. You need {jackets} jackets";
         }
     }
 }
